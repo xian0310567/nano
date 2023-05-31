3701789437e6e885eb66dc34a4be8d6f20bea2cc
the_comma2537 <the_comma2537@naver.com>
2023-05-31 20:38:03 +0900

    크롤링 API 생성

---
+++ src/pages/api/crawl.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import cheerio from "cheerio";
+
+type Data = {
+  price: number;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | string>
+) {
+  if (!req.query.url) return res.status(400).send("not found param: url");
+
+  axios.get(req.query.url + "").then((response) => {
+    const $ = cheerio.load(response.data);
+
+    $("meta").each((_, el) => {
+      const key = $(el).attr("property")?.split(":")[1];
+      if (key == "title") {
+        const title = $(el).attr("content");
+        const price = title?.slice(0, title.indexOf("₩"));
+        if (!price) {
+          res.status(500).send("값을 찾을 수 없습니다.");
+        } else {
+          res.status(200).send({
+            price: parseInt(price),
+          });
+        }
+      }
+    });
+  });
+}
