a77f8e8ecfeefdc3e80f68b8f6b5db75b434dbd1
the_comma2537 <the_comma2537@naver.com>
2023-06-19 00:42:56 +0900

    feat: 특정 페이지 레이아웃 렌더링 설정

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { Layout as ALayout } from "antd";
 
 import Header from "@/components/layout/Header";
@@ -6,6 +7,10 @@ import Content from "@/components/layout/Content";
 import Sider from "@/components/layout/Sider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  if (router.pathname === "/signIn") return <Content>{children}</Content>;
+
   return (
     <ALayout>
       <Header />
